e13f57f88cf913cae37145af4ecf9fe053ac9ed5
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2020-02-10 23:43:25 -0600

    1.1.2.0

--- Source/MoarKerbals.cs
+++ Source/MoarKerbals.cs
@@ -14,10 +14,10 @@ namespace MoarKerbals
         [KSPField]
         public string recipeAmounts;
         [KSPField]
-        public float accidentRate;
+        public double accidentRate;
 
         private string[] resourceList;
-        private float[] resourceAmounts;
+        private double[] resourceAmounts;
         private string[] str_amounts;
 
         protected AudioSource overload;
@@ -27,9 +27,9 @@ namespace MoarKerbals
         {
             resourceList = recipeIngredients.Split(',');
             str_amounts = recipeAmounts.Split(',');
-            resourceAmounts = new float[str_amounts.Length];
+            resourceAmounts = new double[str_amounts.Length];
             for (int i = 0; i < resourceList.Length; i++)
-                resourceAmounts[i] = float.Parse(str_amounts[i]);
+                resourceAmounts[i] = double.Parse(str_amounts[i]);
             if (accidentRate < 0)
                 accidentRate = 0;
             else if (accidentRate > 1)
@@ -53,7 +53,6 @@ namespace MoarKerbals
         }
 
         [KSPEvent(active = true, guiActive = true, guiName = "Initiate Kloning!")]
-        [Obsolete]
         public void ActivateKlone()
         {
             if (PartHasRoom(part) && GatherResources(part))
@@ -66,8 +65,10 @@ namespace MoarKerbals
 
                     if (kerbal.seat != null)
                         kerbal.seat.SpawnCrew();
-                    ScreenMessages.PostScreenMessage("Kloning Success!  " + kerbal.name + " has joined your space program", 3.5f, ScreenMessageStyle.UPPER_CENTER);
-                    kloning_success.Play();
+                
+                    //ScreenMessages.PostScreenMessage("Kloning Success!  " + kerbal.name + " has joined your space program", 3.5f, ScreenMessageStyle.UPPER_CENTER);
+                   Utilities.msg("Kloning Success!  " + kerbal.name + "(Lv " + kerbal.experienceLevel.ToString() + " "+ kerbal.experienceTrait.ToString() + ") has joined your space program");
+                   if (HighLogic.CurrentGame.Parameters.CustomParams<MoarKerbals_Options>().SoundOn) kloning_success.Play();
                 }
                 else {
                     int opt = rnd.Next(0,100);
@@ -81,45 +82,52 @@ namespace MoarKerbals
                     }
                     if (count > 1) {
                         if (opt < 30)
-                            ScreenMessages.PostScreenMessage("A power surge opens a portal to R'lyeh and all your test subjects vanish!", 10f, ScreenMessageStyle.UPPER_CENTER); 
+                            // ScreenMessages.PostScreenMessage("A power surge opens a portal to R'lyeh and all your test subjects vanish!", 10f, ScreenMessageStyle.UPPER_CENTER); 
+                            Utilities.msg("A power surge opens a portal to R'lyeh and all your test subjects vanish!"); 
                         else if (opt < 60)
-                            ScreenMessages.PostScreenMessage("Your Kerbals are lost in the Matrix!", 10f, ScreenMessageStyle.UPPER_CENTER); 
+                            // ScreenMessages.PostScreenMessage("Your Kerbals are lost in the Matrix!", 10f, ScreenMessageStyle.UPPER_CENTER); 
+                           Utilities.msg("Your Kerbals are lost in the Matrix!"); 
                         else if (opt < 85)
-                            ScreenMessages.PostScreenMessage("Radiation turns " + culprit + " into a monstrous creature from the deep who eats your other kerbals before shambling off!", 10f, ScreenMessageStyle.UPPER_CENTER); 
+                            // ScreenMessages.PostScreenMessage("Radiation turns " + culprit + " into a monstrous creature from the deep who eats your other kerbals before shambling off!", 10f, ScreenMessageStyle.UPPER_CENTER); 
+                           Utilities.msg("Radiation turns " + culprit + " into a monstrous creature from the deep who eats your other kerbals before shambling off!"); 
                         else 
-                            ScreenMessages.PostScreenMessage("The Kloning process failed.  This is going to take some cleaning.", 10f, ScreenMessageStyle.UPPER_CENTER); 
+                            // ScreenMessages.PostScreenMessage("The Kloning process failed.  This is going to take some cleaning.", 10f, ScreenMessageStyle.UPPER_CENTER); 
+                           Utilities.msg("The Kloning process failed.  This is going to take some cleaning."); 
                     }
                     else {
                         if (opt < 30)
-                            ScreenMessages.PostScreenMessage(culprit + " comes out with a goatee, yelling something about a Terran Empire.", 10f, ScreenMessageStyle.UPPER_CENTER); 
+                            // ScreenMessages.PostScreenMessage(culprit + " comes out with a goatee, yelling something about a Terran Empire.", 10f, ScreenMessageStyle.UPPER_CENTER); 
+                           Utilities.msg(culprit + " comes out with a goatee, yelling something about a Terran Empire."); 
                         else if (opt < 60)
-                            ScreenMessages.PostScreenMessage(culprit + " mutates into a giant fly, but goes on to have a successful movie career.", 10f, ScreenMessageStyle.UPPER_CENTER); 
+                            // ScreenMessages.PostScreenMessage(culprit + " mutates into a giant fly, but goes on to have a successful movie career.", 10f, ScreenMessageStyle.UPPER_CENTER); 
+                           Utilities.msg(culprit + " mutates into a giant fly, but goes on to have a successful movie career."); 
                         else if (opt < 80)
-                            ScreenMessages.PostScreenMessage("You find the bay empty, a note pinned to the wall 'Gone back to the future'", 10f, ScreenMessageStyle.UPPER_CENTER); 
+                            // ScreenMessages.PostScreenMessage("You find the bay empty, a note pinned to the wall 'Gone back to the future'", 10f, ScreenMessageStyle.UPPER_CENTER); 
+                           Utilities.msg("You find the bay empty, a note pinned to the wall 'Gone back to the future'"); 
                         else
-                            ScreenMessages.PostScreenMessage("The Kloning process failed.  You lost " + culprit + ", but at least you have pizza now.", 10f, ScreenMessageStyle.UPPER_CENTER); 
+                            // ScreenMessages.PostScreenMessage("The Kloning process failed.  You lost " + culprit + ", but at least you have pizza now."); 
+                           Utilities.msg("The Kloning process failed.  You lost " + culprit + ", but at least you have pizza now."); 
                     }
-                    overload.Play();
+                    if (HighLogic.CurrentGame.Parameters.CustomParams<MoarKerbals_Options>().SoundOn) overload.Play();
                 }
             }
             GameEvents.onVesselChange.Fire(FlightGlobals.ActiveVessel);
         }
 
-        [Obsolete]
         private bool GatherResources(Part part)
         {
             //Steps through to gather resources
             for (int i = 0; i < resourceList.Length; i++)
             {
-                float available = part.RequestResource(resourceList[i], resourceAmounts[i]);
+                double available = part.RequestResource(resourceList[i], (resourceAmounts[i] * HighLogic.CurrentGame.Parameters.CustomParams<MoarKerbals_Options>().globalKloningCostMultiplier));
 
-                if (available != resourceAmounts[i])
+                if (available != (resourceAmounts[i] * HighLogic.CurrentGame.Parameters.CustomParams<MoarKerbals_Options>().globalKloningCostMultiplier))
                 {
                     //Upon not having enough of a resource, returns all previously collected
-                    part.RequestResource(resourceList[i], -available);
+                    part.RequestResource(resourceList[i], (-available * HighLogic.CurrentGame.Parameters.CustomParams<MoarKerbals_Options>().globalKloningCostMultiplier));
                     for (int j = 0; j < i; j++)
-                        part.RequestResource(resourceList[j], -resourceAmounts[j]);
-                    ScreenMessages.PostScreenMessage("Insufficient " + resourceList[i] + " to start Kloning (" + available.ToString() + "/" + resourceAmounts[i].ToString() + ")", 3.5f, ScreenMessageStyle.UPPER_CENTER);
+                        part.RequestResource(resourceList[j], (-resourceAmounts[j] * HighLogic.CurrentGame.Parameters.CustomParams<MoarKerbals_Options>().globalKloningCostMultiplier));
+                    ScreenMessages.PostScreenMessage("Insufficient " + resourceList[i] + " to start Kloning (" + available.ToString() + "/" + (resourceAmounts[i] * HighLogic.CurrentGame.Parameters.CustomParams<MoarKerbals_Options>().globalKloningCostMultiplier).ToString() + ")", 3.5f, ScreenMessageStyle.UPPER_CENTER);
                     return false;
                 }
             }
@@ -129,7 +137,7 @@ namespace MoarKerbals
         //Checks to make sure there is at least one kerbal as a DNA source and that there is room to store the new kerbal
         private bool PartHasRoom(Part part)
         {
-            if ((part.protoModuleCrew.Count < part.CrewCapacity) && (part.protoModuleCrew.Count > 0))
+            if ((part.protoModuleCrew.Count < part.CrewCapacity) && ((part.protoModuleCrew.Count > 0) || HighLogic.CurrentGame.Parameters.CustomParams<MoarKerbals_Options>().RequireLivingKerbal))
                 return true;
             else
             {
@@ -145,10 +153,10 @@ namespace MoarKerbals
         {
             string[] resList = recipeIngredients.Split(',');
             string[] amounts = recipeAmounts.Split(',');
-            float[] resAmounts = new float[amounts.Length];
+            double[] resAmounts = new double[amounts.Length];
 
             for (int i = 0; i < resList.Length; i++)
-                resAmounts[i] = float.Parse(amounts[i]);
+                resAmounts[i] = double.Parse(amounts[i]);
 
             string display = "\r\nInput:\r\n";
             for (int i = 0; i < resList.Length; i++)
@@ -171,20 +179,19 @@ namespace MoarKerbals
         public bool accidentRate;
 
         private string[] resourceList;
-        private float[] resourceAmounts;
+        private double[] resourceAmounts;
         private string[] str_amounts;
 
         public override void OnStart(PartModule.StartState state)
         {
             resourceList = recipeIngredients.Split(',');
             str_amounts = recipeAmounts.Split(',');
-            resourceAmounts = new float[str_amounts.Length];
+            resourceAmounts = new double[str_amounts.Length];
             for (int i = 0; i < resourceList.Length; i++)
-                resourceAmounts[i] = float.Parse(str_amounts[i]);
+                resourceAmounts[i] = double.Parse(str_amounts[i]);
         }
 
         [KSPEvent(active = true, guiActive = true, guiName = "Initiate Kloning!")]
-        [Obsolete]
         public void ActivateKlone()
         {
             if (PartHasRoom(part) && GatherResources(part))
@@ -200,13 +207,12 @@ namespace MoarKerbals
             }
         }
 
-        [Obsolete]
         private bool GatherResources(Part part)
         {
             //Steps through to gather resources
             for (int i = 0; i < resourceList.Length; i++)
             {
-                float available = part.RequestResource(resourceList[i], resourceAmounts[i]);
+                double available = part.RequestResource(resourceList[i], resourceAmounts[i]);
                 //debug:
                 print(" DEBUG: " + available.ToString());
                 if (available != resourceAmounts[i])
@@ -225,7 +231,7 @@ namespace MoarKerbals
         //Checks to make sure there is at least one kerbal as a DNA source and that there is room to store the new kerbal
         private bool PartHasRoom(Part part)
         {
-            if ((part.protoModuleCrew.Count < part.CrewCapacity) && (part.protoModuleCrew.Count > 0))
+            if ((part.protoModuleCrew.Count < part.CrewCapacity) && ((part.protoModuleCrew.Count > 0) || HighLogic.CurrentGame.Parameters.CustomParams<MoarKerbals_Options>().RequireLivingKerbal))
                 return true;
             else
             {
@@ -241,10 +247,10 @@ namespace MoarKerbals
         {
             string[] resList = recipeIngredients.Split(',');
             string[] amounts = recipeAmounts.Split(',');
-            float[] resAmounts = new float[amounts.Length];
+            double[] resAmounts = new double[amounts.Length];
 
             for (int i = 0; i < resList.Length; i++)
-                resAmounts[i] = float.Parse(amounts[i]);
+                resAmounts[i] = double.Parse(amounts[i]);
 
             string display = "\r\nInput:\r\n";
             for (int i = 0; i < resList.Length; i++)
--- Source/Utilities.cs
+++ Source/Utilities.cs
@@ -35,6 +35,13 @@ namespace MoarKerbals
 {
     class Utilities
     {
+        internal static void msg(string msg, params object[] @params)
+        {
+            if (0 != @params.Length) msg = string.Format(msg, @params);
+            ScreenMessages.PostScreenMessage(msg, 10f, ScreenMessageStyle.UPPER_CENTER, true);
+            if (HighLogic.CurrentGame.Parameters.CustomParams<MoarKerbals_Options>().InGameMail) UnityEngine.Debug.Log("[MoarKerbals] " + msg);
+        }
+
         public static string GetDllVersion<T>(T t)
         {
             System.Reflection.Assembly assembly = t.GetType().Assembly;
@@ -68,10 +75,10 @@ namespace MoarKerbals
             }
         }
 
-        public static float GetValue(ConfigNode config, string name, float currentValue)
+        public static double GetValue(ConfigNode config, string name, double currentValue)
         {
-            float newValue;
-            if (config.HasValue(name) && float.TryParse(config.GetValue(name), out newValue))
+            double newValue;
+            if (config.HasValue(name) && double.TryParse(config.GetValue(name), out newValue))
             {
                 return newValue;
             }
@@ -81,10 +88,10 @@ namespace MoarKerbals
             }
         }
 
-        public static double GetValue(ConfigNode config, string name, double currentValue)
+        public static float GetValue(ConfigNode config, string name, float currentValue)
         {
-            double newValue;
-            if (config.HasValue(name) && double.TryParse(config.GetValue(name), out newValue))
+            float newValue;
+            if (config.HasValue(name) && float.TryParse(config.GetValue(name), out newValue))
             {
                 return newValue;
             }
