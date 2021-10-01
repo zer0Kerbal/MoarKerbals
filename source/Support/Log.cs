6aaf98223fc8de8e4ea0d81ee215778b69fd42e4
zer0Kerbal <39887717+zer0Kerbal@users.noreply.github.com>
2021-10-01 01:32:56 -0500

    Create Log.cs

---
+++ source/Support/Log.cs
@@ -0,0 +1,30 @@
+﻿#if false
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace MoarKerbals
+{
+    class Log
+    {
+        /// <summary>
+        /// sends the specific message to ingame mail and screen if Debug is defined
+        /// For debugging use only.
+        /// </summary>
+        /// <param name="msg">The MSG.</param>
+        /// <param name="params">The parameters.</param>
+        [ConditionalAttribute("DEBUG")]
+        internal static void dbg(string msg, params object[] @params)
+        {
+#if DEBUG
+            if (0 != @params.Length) msg = string.Format(msg, @params);
+            ScreenMessages.PostScreenMessage(msg, 1, ScreenMessageStyle.UPPER_CENTER, true);
+            if (SettingsInterface.InGameMail()) UnityEngine.Debug.Log("[MoarKerbals] " + msg);
+#endif
+        }
+    }
+}
+#endif
